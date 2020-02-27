9b22876858b01e499b5eafaef836e710342704c6
Alexandre Bourget <alex@bourget.cc>
2020-02-27 12:10:36 -0500

    First revision of this beautiful library. Now roooooll it out everywhere!

---
+++ README.md
@@ -0,0 +1,11 @@
+dfuse Service Definitions Library for Go
+----------------------------------------
+
+This has generated code from the `service-definitions` repository for Go.
+
+DO NOT import third-party dependencies in here (like the different chain's toolkits).
+
+The `go.mod` within needs to be kept to an absolute minimum.
+
+You can add helpers but only inasmuch they help using the PB
+definitions, standard-library and thin thin general purpose helpers.
---
+++ generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+current_dir="`pwd`"
+trap "cd \"$current_dir\"" EXIT
+pushd "$ROOT" &> /dev/null
+
+# Service definitions
+SERVICES=${1:-../service-definitions}
+
+protoc -I$SERVICES grpc/health/v1/health.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/abicodec/eosio/v1/abicodec.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/blockmeta/v1/blockmeta.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/graphql/v1/graphql.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/headinfo/v1/headinfo.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/merger/v1/merger.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/search/v1/search.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/tokenmeta/v1/tokenmeta.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/trxstatetracker/v1/trxstatetracker.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/bstream/v1/bstream.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/codecs/deos/deos.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I$SERVICES dfuse/codecs/deth/deth.proto --go_out=plugins=grpc,paths=source_relative:.
+
+echo "generate.sh - `date` - `whoami`" > last_generate.txt
+echo -n "service-definitions revision: " >> last_generate.txt
+GIT_DIR=$SERVICES/.git  git rev-parse HEAD >> last_generate.txt
