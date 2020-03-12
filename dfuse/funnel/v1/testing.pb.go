b68a74fe34e871cc8fe7b0eb6ee0f357f2297329
Francois Proulx <francois@eoscanada.com>
2020-03-12 00:56:04 -0400

    Added testing code for funnel

---
+++ dfuse/funnel/v1/testing.pb.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	"context"
+	"io"
+
+	grpc "google.golang.org/grpc"
+	deos "github.com/dfuse-io/pbgo/dfuse/codecs/deos"
+)
+
+type TestFunnelClient struct {
+	elements []interface{}
+}
+
+func NewTestFunnelClient(elements []interface{}) *TestFunnelClient {
+	return &TestFunnelClient{elements: elements}
+}
+
+func (c *TestFunnelClient) StreamBlocks(ctx context.Context, in *FunnelRequest, opts ...grpc.CallOption) (Funnel_StreamBlocksClient, error) {
+	return &TestStreamBlocksClient{elements: c.elements}, nil
+}
+
+type TestStreamBlocksClient struct {
+	elements []interface{}
+	idx      int
+
+	grpc.ClientStream
+}
+
+func (c *TestStreamBlocksClient) Recv() (*deos.Block, error) {
+	defer func() { c.idx++ }()
+
+	if c.idx >= len(c.elements) {
+		return nil, io.EOF
+	}
+
+	switch out := c.elements[c.idx].(type) {
+	case *deos.Block:
+		return out, nil
+	case error:
+		return nil, out
+	default:
+		panic("unsupported typed")
+	}
+}
