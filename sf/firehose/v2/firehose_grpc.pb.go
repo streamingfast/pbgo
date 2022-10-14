3a05d7bc30c8051d88b7d7c80fe4b3827cc5fdfb
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-10-14 15:16:46 -0400

    add Fetch service on firehose for single Block

--- sf/firehose/v2/firehose_grpc.pb.go
+++ sf/firehose/v2/firehose_grpc.pb.go
@@ -128,3 +128,87 @@ var Stream_ServiceDesc = grpc.ServiceDesc{
 	},
 	Metadata: "sf/firehose/v2/firehose.proto",
 }
+
+// FetchClient is the client API for Fetch service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type FetchClient interface {
+	Block(ctx context.Context, in *SingleBlockRequest, opts ...grpc.CallOption) (*SingleBlockResponse, error)
+}
+
+type fetchClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewFetchClient(cc grpc.ClientConnInterface) FetchClient {
+	return &fetchClient{cc}
+}
+
+func (c *fetchClient) Block(ctx context.Context, in *SingleBlockRequest, opts ...grpc.CallOption) (*SingleBlockResponse, error) {
+	out := new(SingleBlockResponse)
+	err := c.cc.Invoke(ctx, "/sf.firehose.v2.Fetch/Block", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// FetchServer is the server API for Fetch service.
+// All implementations should embed UnimplementedFetchServer
+// for forward compatibility
+type FetchServer interface {
+	Block(context.Context, *SingleBlockRequest) (*SingleBlockResponse, error)
+}
+
+// UnimplementedFetchServer should be embedded to have forward compatible implementations.
+type UnimplementedFetchServer struct {
+}
+
+func (UnimplementedFetchServer) Block(context.Context, *SingleBlockRequest) (*SingleBlockResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Block not implemented")
+}
+
+// UnsafeFetchServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to FetchServer will
+// result in compilation errors.
+type UnsafeFetchServer interface {
+	mustEmbedUnimplementedFetchServer()
+}
+
+func RegisterFetchServer(s grpc.ServiceRegistrar, srv FetchServer) {
+	s.RegisterService(&Fetch_ServiceDesc, srv)
+}
+
+func _Fetch_Block_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SingleBlockRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FetchServer).Block(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/sf.firehose.v2.Fetch/Block",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FetchServer).Block(ctx, req.(*SingleBlockRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Fetch_ServiceDesc is the grpc.ServiceDesc for Fetch service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Fetch_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "sf.firehose.v2.Fetch",
+	HandlerType: (*FetchServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Block",
+			Handler:    _Fetch_Block_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "sf/firehose/v2/firehose.proto",
+}
