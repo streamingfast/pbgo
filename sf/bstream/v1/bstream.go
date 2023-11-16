b8f9cdce6ef80d96e346553e21c8b79835b36752
Julien Cassis <juliencassis@gmail.com>
2023-11-16 09:27:00 -0500

    Added helpers

--- sf/bstream/v1/bstream.go
+++ sf/bstream/v1/bstream.go
@@ -1,18 +1,65 @@
 package pbbstream
 
 import (
+	"fmt"
 	"time"
 )
 
-// GetFirehoseBlockID implements firecore.Block.
-func (b *Block) GetFirehoseBlockID() string {
-	return b.Id
+func (b *Block) Time() time.Time {
+	if b == nil {
+		return time.Time{}
+	}
+	if err := b.Timestamp.CheckValid(); err != nil {
+		panic(fmt.Errorf("invalid timestamp: %w", err))
+	}
+
+	return b.Timestamp.AsTime()
 }
 
-// GetFirehoseBlockNumber implements firecore.Block.
-func (b *Block) GetFirehoseBlockNumber() uint64 {
-	return b.Number
+func (b *Block) LIBNum() uint64 {
+	if b == nil {
+		return 0
+	}
+
+	return b.LibNum
 }
+
+type basicBlockRef struct {
+	id  string
+	num uint64
+}
+
+func (e *basicBlockRef) Num() uint64 { return e.num }
+func (e *basicBlockRef) ID() string  { return e.id }
+func (e *basicBlockRef) String() string {
+	if e == nil || (e.id == "" && e.num == 0) {
+		return "Block <nil>"
+	}
+
+	return fmt.Sprintf("#%d (%s)", e.Num(), e.ID())
+}
+
+func (b *Block) AsRef() *basicBlockRef {
+	if b == nil {
+		return &basicBlockRef{"", 0}
+	}
+
+	return &basicBlockRef{b.Id, b.Number}
+}
+
+func (b *Block) PreviousRef() *basicBlockRef {
+	if b == nil || b.PreviousNum == 0 || b.PreviousId == "" {
+		return &basicBlockRef{"", 0}
+	}
+	return &basicBlockRef{b.PreviousId, b.PreviousNum}
+}
+
+func (b *Block) PreviousID() string {
+	return b.PreviousRef().ID()
+}
+
+func (b *Block) GetFirehoseBlockID() string     { return b.Id }
+func (b *Block) GetFirehoseBlockNumber() uint64 { return b.Number }
 
 // GetFirehoseBlockParentID implements firecore.Block.
 func (b *Block) GetFirehoseBlockParentID() string {
@@ -21,7 +68,7 @@ func (b *Block) GetFirehoseBlockParentID() string {
 
 // GetFirehoseBlockParentNumber implements firecore.Block.
 func (b *Block) GetFirehoseBlockParentNumber() uint64 {
-	panic("implement me")
+	return b.PreviousNum
 }
 
 // GetFirehoseBlockTime implements firecore.Block.
@@ -32,5 +79,4 @@ func (b *Block) GetFirehoseBlockTime() time.Time {
 // GetFirehoseBlockVersion implements firecore.Block.
 func (b *Block) GetFirehoseBlockVersion() int32 {
 	panic("implement me, not sure this is the right value to return")
-	return b.PayloadVersion
 }
